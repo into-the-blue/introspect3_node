91112961ba8bfbf35d3e72097558209a3ae024e8
Origami <q@timvel.com>
2022-02-14 10:35:59 +0800

    feat: add docker, sentry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { SentryModule } from '@ntegral/nestjs-sentry';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImageModule } from './api/image/image.module';
 @Module({
-  imports: [ImageModule],
+  imports: [
+    SentryModule.forRoot({
+      dsn: process.env.SENTRY_DSN,
+      debug: true,
+      environment: process.env.ENV,
+      release: process.env.RELEASE, // must create a release in sentry.io dashboard
+      logLevels: ['debug'], //based on sentry.io loglevel
+    }),
+    ImageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
