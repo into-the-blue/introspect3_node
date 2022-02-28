26672e10d9d74a2e4039e903b5d8846973fe55a5
Origami <q@timvel.com>
2022-02-28 21:37:06 +0800

    unsplash setup

--- src/utils/unsplash.ts
+++ src/utils/unsplash.ts
@@ -1,7 +1,10 @@
-import nodeFetch from 'node-fetch';
 import { createApi } from 'unsplash-js';
+import { RequestInfo, RequestInit } from 'node-fetch';
+
+const fetch = (url: RequestInfo, init?: RequestInit) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(url, init));
 
 export const Unsplash = createApi({
-  fetch: nodeFetch as any,
+  fetch: fetch as any,
   accessKey: process.env.UNSPLASH_ACCESS_KEY,
 });
